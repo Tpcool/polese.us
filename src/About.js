43e065b195498771df52e8dfd89a8830a7bad73e
Thomas Polese <thomas@polese.us>
2021-09-01 03:54:23 -0400

    About section separator

--- src/About.js
+++ src/About.js
@@ -12,9 +12,11 @@ class AboutSection extends Component {
                 <section class="p-4">
                     <div class="container">
                         <h2>{this.props.header}</h2>
+                        <div class="row">
                             <div class="col-md-12">
-                                <img class="m-4 rounded float-start img-fluid" src={this.props.image} alt={this.props.imageAltText} />
-                                <p class="lead my-3">{this.props.body}</p>
+                                <img class={IsOdd(this.props.index) ? "fit-image img-fluid m-4 rounded float-start" : "fit-image img-fluid m-4 rounded float-end"} src={this.props.image} alt={this.props.imageAltText} />
+                                <p class="lead my-3">{this.props.body} aaaand {this.props.index}</p>
+                            </div>
                         </div>
                     </div>
                 </section>
@@ -23,13 +25,31 @@ class AboutSection extends Component {
     }
 }
 
+// Main section, then nested subsection. Main section contains section tag and row separator and image, subsection contains subheaders and content
+class AboutSubSection extends Component {
+    render() {
+        return (
+            <>
+                <h1>placeholder</h1>
+            </>
+        );
+    }
+}
+
+function IsOdd(num) {
+    if (num % 2 === 1) return true;
+    else return false;
+}
+
 function About() {
     return (
         <>
-            <h1>Lorem Ipsum</h1>
-            {AboutPageItems.map((item) => {
+            <div class="container pt-4">
+                <h1>Lorem Ipsum</h1>
+            </div>
+            {AboutPageItems.map((item, index) => {
                 return (
-                    <AboutSection header={item.header} image={item.image} imageAltText={item.imageAltText} body={item.body}/>
+                    <AboutSection header={item.header} image={item.image} imageAltText={item.imageAltText} body={item.body} index={index}/>
                 )
             })}
         </>
